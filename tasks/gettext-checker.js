0c4790adee25180fc50bae5a3e2021090c12a9fe
Graham Gilchrist <graham@incuna.com>
2015-03-09 09:53:55 +0000

    Add check for keys being in the same order in .pot and .po files

--- tasks/gettext-checker.js
+++ tasks/gettext-checker.js
@@ -52,7 +52,7 @@ module.exports = function (grunt) {
             return _.map(items, function (item) {
                 return item.msgid;
             });
-        }
+        };
 
         // get pot file keys as array
         var potKeys = getPoKeys(potFile.items);
@@ -63,20 +63,43 @@ module.exports = function (grunt) {
         // find items in template.pot that are not in this po file
         var keyDiff = _.difference(potKeys, poKeys);
         if (keyDiff.length > 0 ) {
-            grunt.log.error('The following translation keys in template.pot are not present in ' + options.poFile);
+            grunt.log.error('The following translation keys in ' + options.potFile + ' are not present in ' + options.poFile);
             grunt.log.error(keyDiff);
             grunt.fail.fatal('Check FAILED.');
         }
 
         // find items in this po file which are not in template.pot
-        var keyDiff = _.difference(poKeys, potKeys);
+        keyDiff = _.difference(poKeys, potKeys);
         if (keyDiff.length > 0 ) {
-            grunt.log.error('The following translation keys in ' + options.poFile + ' are not present in template.pot');
+            grunt.log.error('The following translation keys in ' + options.poFile + ' are not present in ' + options.potFile);
             grunt.log.error(keyDiff);
             grunt.fail.fatal('Check FAILED.');
         }
 
-        grunt.log.ok('Translation .pot template and all .po file keys match.')
+        grunt.log.ok('Translation .pot template and all .po file keys match.');
+
+        // Check if keys in .pot file and .po file are in same order. We use _.find to drop out as soon as we find one non-matching
+        // to make things a bit faster if we can
+        var matchingOrder = _.find(potKeys, function (item, index) {
+            if (poKeys[index] !== item) {
+                // The keys don't match, so are out of order
+                return true;
+            }
+            return false;
+        });
+        // _.find returns undefined if all keys are in matching order, so we error if it is not undefined
+        if (!_.isUndefined(matchingOrder)) {
+            grunt.log.error(
+                'The keys in ' + options.potFile + ' and ' + options.poFile + ' are not in the same order.\r\n' +
+                'If you were not expecting the order to change, please check your generating tool version and ' +
+                'environment to check it matches the environment used by the project.\r\n' +
+                'If you have deliberately changed the order of the .pot file, you must update the order of the ' +
+                '.po files as well - e.g. Using a tool such as poEdit\`s "Update from template" option.'
+            );
+            grunt.fail.fatal('Check FAILED.');
+        } else {
+            grunt.log.ok('Translation .pot template and all .po file keys are in the same order.');
+        }
 
     });
 };
